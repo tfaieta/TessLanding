61b92cfb535b9ccdc27b7c83242867a4c1da4710
Tony Faieta <tfaieta@gmail.com>
2018-09-23 20:54:01 -0400

    some more formatting

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,14 +4,10 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>Tess</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
-                <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
-                </ul>
+                <p>Free and powerful cross-platform podcast player.</p>
             </div>
         </div>
     </section>
