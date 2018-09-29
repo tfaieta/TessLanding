982c0d69a15b2450b4f1b717e8962a266271213a
Tony Faieta <tfaieta@gmail.com>
2018-09-28 22:01:05 -0400

    more

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,61 +65,54 @@ class Index extends React.Component {
             </header>
             <ul className="features">
               <li>
-                <span className="icon major style1 fa-code"></span>
-                <h3>Amed sed feugiat</h3>
-                <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>                
-              </li>
-              <li>
-                <span className="icon major style3 fa-copy"></span>
-                <h3>Use it everywhere</h3>
+                <span className="icon major style3 fa-magic"></span>
+                <h3>Use it Everywhere</h3>
                 <p>Tess works on iPhone, Android, and and the web, 
-                so you can listen wherever you are. One login let's you jump between devices.</p>
+                so you can listen to all your favorite podcasts wherever you are.</p>
               </li>
               <li>
-                <span className="icon major style5 fa-diamond"></span>
-                <h3>Dolor nullam</h3>
-                <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>
+                <span className="icon major style1 fa-bookmark-o"></span>
+                <h3>Highlight and Share</h3>
+                <p>Save highlights of podcasts to your library for later listening or share a highlight with
+                others!
+                </p>                
+              </li>
+              <li>
+                <span className="icon major style5 fa-code"></span>
+                <h3>Open Source</h3>
+                <p>Through open source, anyone can propose new features and make Tess better.</p>
               </li>
             </ul>
-            <footer className="major">
-              <ul className="actions">
-                <li><Link to="/generic" className="button">Learn More</Link></li>
-              </ul>
-            </footer>
           </section>
 
           <section id="second" className="main special">
             <header className="major">
-              <h2>Ipsum consequat</h2>
-              <p>Donec imperdiet consequat consequat. Suspendisse feugiat congue<br />
-              posuere. Nulla massa urna, fermentum eget quam aliquet.</p>
+              <h2>For developers</h2>
+              <p>Tess uses React Native for it's iOS/Android app,<br/>
+              React for its Web player, and all backend is managed through Firebase.
+              </p>
             </header>
             <ul className="statistics">
               <li className="style1">
-                <span className="icon fa-code-fork"></span>
-                <strong>5,120</strong> Etiam
+                <span className="icon fa-code"></span>
+                <strong>35k</strong> Lines
               </li>
               <li className="style2">
-                <span className="icon fa-folder-open-o"></span>
-                <strong>8,192</strong> Magna
+                <span className="icon fa-rss"></span>
+                <strong>200+</strong>Podcasts
               </li>
               <li className="style3">
-                <span className="icon fa-signal"></span>
-                <strong>2,048</strong> Tempus
-              </li>
-              <li className="style4">
-                <span className="icon fa-laptop"></span>
-                <strong>4,096</strong> Aliquam
-              </li>
-              <li className="style5">
-                <span className="icon fa-diamond"></span>
-                <strong>1,024</strong> Nullam
+                <span className="icon fa-users"></span>
+                <strong>100+</strong>Users
               </li>
             </ul>
-            <p className="content">Nam elementum nisl et mi a commodo porttitor. Morbi sit amet nisl eu arcu faucibus hendrerit vel a risus. Nam a orci mi, elementum ac arcu sit amet, fermentum pellentesque et purus. Integer maximus varius lorem, sed convallis diam accumsan sed. Etiam porttitor placerat sapien, sed eleifend a enim pulvinar faucibus semper quis ut arcu. Ut non nisl a mollis est efficitur vestibulum. Integer eget purus nec nulla mattis et accumsan ut magna libero. Morbi auctor iaculis porttitor. Sed ut magna ac risus et hendrerit scelerisque. Praesent eleifend lacus in lectus aliquam porta. Cras eu ornare dui curabitur lacinia.</p>
+            <p>
+            Tess is looking for a lot of help from podcast lovers looking to make a better free podcast app.
+            If you would like to talk more about Tess or become a maintainer you can join our Discord server below.
+            </p>
             <footer className="major">
               <ul className="actions">
-                <li><Link to="/generic" className="button">Learn More</Link></li>
+                <a href="https://discord.gg/JuucUTc" className="button">Join on Discord!</a>
               </ul>
             </footer>
           </section>
