50c72b013211249c3821f9b8ce57b58042bbbe1e
Tony Faieta <tfaieta@gmail.com>
2018-09-30 16:05:17 -0400

    Published

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -8,6 +8,7 @@ exports.layouts = {
 
 exports.components = {
   "component---src-templates-blog-post-js": preferDefault(require("/Users/TonyFaieta/TessLanding/src/templates/blog-post.js")),
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/TonyFaieta/TessLanding/.cache/dev-404-page.js")),
   "component---src-pages-generic-js": preferDefault(require("/Users/TonyFaieta/TessLanding/src/pages/generic.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/TonyFaieta/TessLanding/src/pages/index.js"))
 }
@@ -17,6 +18,7 @@ exports.json = {
   "hello-world.json": require("/Users/TonyFaieta/TessLanding/.cache/json/hello-world.json"),
   "my-second-post.json": require("/Users/TonyFaieta/TessLanding/.cache/json/my-second-post.json"),
   "hi-folks.json": require("/Users/TonyFaieta/TessLanding/.cache/json/hi-folks.json"),
+  "dev-404-page.json": require("/Users/TonyFaieta/TessLanding/.cache/json/dev-404-page.json"),
   "generic.json": require("/Users/TonyFaieta/TessLanding/.cache/json/generic.json"),
   "index.json": require("/Users/TonyFaieta/TessLanding/.cache/json/index.json")
 }
