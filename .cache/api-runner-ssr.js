321aee724f58ba89b1390b2fcd5a15a08c1114f6
Tony Faieta <tfaieta@gmail.com>
2019-01-12 23:42:02 -0500

    Added smarticles

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,5 +1,5 @@
 var plugins = [{
-      plugin: require('/Users/TonyFaieta/TessLanding/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/Users/pierofaieta/TessLanding/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,20 +3,20 @@ const preferDefault = m => m && m.default || m
 
 
 exports.layouts = {
-  "layout---index": preferDefault(require("/Users/TonyFaieta/TessLanding/.cache/layouts/index.js"))
+  "layout---index": preferDefault(require("/Users/pierofaieta/TessLanding/.cache/layouts/index.js"))
 }
 
 exports.components = {
-  "component---src-templates-blog-post-js": preferDefault(require("/Users/TonyFaieta/TessLanding/src/templates/blog-post.js")),
-  "component---src-pages-generic-js": preferDefault(require("/Users/TonyFaieta/TessLanding/src/pages/generic.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/TonyFaieta/TessLanding/src/pages/index.js"))
+  "component---src-templates-blog-post-js": preferDefault(require("/Users/pierofaieta/TessLanding/src/templates/blog-post.js")),
+  "component---src-pages-generic-js": preferDefault(require("/Users/pierofaieta/TessLanding/src/pages/generic.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/pierofaieta/TessLanding/src/pages/index.js"))
 }
 
 exports.json = {
-  "layout-index.json": require("/Users/TonyFaieta/TessLanding/.cache/json/layout-index.json"),
-  "hello-world.json": require("/Users/TonyFaieta/TessLanding/.cache/json/hello-world.json"),
-  "my-second-post.json": require("/Users/TonyFaieta/TessLanding/.cache/json/my-second-post.json"),
-  "hi-folks.json": require("/Users/TonyFaieta/TessLanding/.cache/json/hi-folks.json"),
-  "generic.json": require("/Users/TonyFaieta/TessLanding/.cache/json/generic.json"),
-  "index.json": require("/Users/TonyFaieta/TessLanding/.cache/json/index.json")
+  "layout-index.json": require("/Users/pierofaieta/TessLanding/.cache/json/layout-index.json"),
+  "hi-folks.json": require("/Users/pierofaieta/TessLanding/.cache/json/hi-folks.json"),
+  "my-second-post.json": require("/Users/pierofaieta/TessLanding/.cache/json/my-second-post.json"),
+  "hello-world.json": require("/Users/pierofaieta/TessLanding/.cache/json/hello-world.json"),
+  "generic.json": require("/Users/pierofaieta/TessLanding/.cache/json/generic.json"),
+  "index.json": require("/Users/pierofaieta/TessLanding/.cache/json/index.json")
 }
